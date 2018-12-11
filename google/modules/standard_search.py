0549796896016bcd24117ba98aa6377e11891a44
tstanislawek <tomasz.stanislawek@applica.pl>
2018-12-11 19:43:00 +0100

    Add support for site parameter in query method

--- google/modules/standard_search.py
+++ google/modules/standard_search.py
@@ -51,7 +51,7 @@ class GoogleResult(object):
 
 
 # PUBLIC
-def search(query, pages=1, lang='en', area='com', ncr=False, void=True, time_period=False):
+def search(query, pages=1, lang='en', area='com', ncr=False, void=True, time_period=False, site=False):
     """Returns a list of GoogleResult.
 
     Args:
@@ -65,7 +65,7 @@ def search(query, pages=1, lang='en', area='com', ncr=False, void=True, time_per
 
     results = []
     for i in range(pages):
-        url = _get_search_url(query, i, lang=lang, area=area, ncr=ncr, time_period=time_period)
+        url = _get_search_url(query, i, lang=lang, area=area, ncr=ncr, time_period=time_period, site=site)
         html = get_html(url)
 
         if html:
