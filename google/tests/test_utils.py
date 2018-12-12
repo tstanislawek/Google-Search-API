b07732d0404011e5dbb1c1162ae76d317b86e8b0
tstanislawek <tomasz.stanislawek@applica.pl>
2018-12-12 21:28:14 +0100

    Add comments + fix tests

--- google/tests/test_utils.py
+++ google/tests/test_utils.py
@@ -16,14 +16,14 @@ class UtilsTestCase(unittest.TestCase):
     """Tests for helper methods."""
     def test_get_search_url(self):
         url = _get_search_url("apple", 0, 10, "en", "jp")
-        exp_url = "http://www.google.co.jp/search?q=apple&start=0&num=10&hl=en"
-        self.assertEqual(url, exp_url)
+        exp_url = "www.google.co.jp/search?q=apple&start=0&num=10&hl=en"
+        self.assertEqual(url.replace("http://", "").replace("https://", ""), exp_url)
 
     """Tests site functionality for helper methods."""
     def test_extend_get_search_url(self):
         url = _get_search_url("apple", 0, 10, "en", ncr=True, site="apple.ch")
-        exp_url = "http://www.google.ch/search?q=apple+site%3Aapple.ch&start=0&num=10&hl=en&gl=us&pws=0&gws_rd=cr"
-        self.assertEqual(url, exp_url)
+        exp_url = "www.google.ch/search?q=apple+site%3Aapple.ch&start=0&num=10&hl=en&gl=us&pws=0&gws_rd=cr"
+        self.assertEqual(url.replace("http://", "").replace("https://", ""), exp_url)
 
 
 if __name__ == '__main__':
